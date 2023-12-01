cdd5b93243b41bf9ebdf238daeda8f7071ef3c0a
Cortard <119619810+Cortard@users.noreply.github.com>
2023-12-01 17:13:31 +0100

    Day 1 Q 1

--- Day1/main.c
+++ Day1/main.c
@@ -1,18 +1,56 @@
 #include <stdio.h>
+#include <malloc.h>
+
+typedef struct Link Link;
+struct Link {
+    int value;
+    struct Link* next;
+};
+
+void delete(Link* link){
+    if(! link->next) return;
+    delete(link->next);
+    free(link->next);
+}
 
 int main() {
     FILE *stream;
     stream= fopen("../input.txt", "r");
+    if(!stream) return -1;
+    printf("File open\n");
+    
+    char letter;
+    Link first={0,0};
+    Link* current=&first;
+    while(fread(&letter,1,1,stream)){
+        if(48<=letter && letter<=57) {
+            if(current->value<10)
+                current->value = (letter-48)*10;
+            else
+                current->value+=-current->value%10+letter-48;
+        }else if(letter==10){
+            if( current->value%10==0 ) current->value+=current->value/10;
+            Link* _tmp= calloc(1,sizeof(FILE));
+            current->next=_tmp;
+            current=_tmp;
+        }
+    }
+    if( current->value%10==0 ) current->value+=current->value/10;//Last one
 
-    char buffer[100];
-    FILE* end;
-    fseek(filePtr,0,SEEK_END);
+    fclose(stream);
+    printf("Read done\n");
 
-    int size=filePtr->_bufsiz;
-    printf("%d\n",size);
-    char file[size];
-    fgets(file, size, filePtr);
+    current=&first;
+    int sum=0;
+    while (current->next) {
+        sum += current->value;
+        current = current->next;
+    }
+    sum += current->value; //last one
 
-    printf("%s",file);
+    printf("Total sum : %d\n",sum);
+
+    delete(&first);
+
     return 0;
 }
