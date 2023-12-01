58a0b2ddab34b255dd3458fe45a6a215b7dc2c90
Cortard <119619810+Cortard@users.noreply.github.com>
2023-12-01 21:16:26 +0100

    Day 1 Q 2
    Maybe there is more fonc in c that what I was thinking xD

--- Day1/main.c
+++ Day1/main.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include <malloc.h>
+#include <string.h>
 
 typedef struct Link Link;
 struct Link {
     int value;
     struct Link* next;
 };
-
 void delete(Link* link){
     if(! link->next) return;
     delete(link->next);
@@ -18,25 +17,42 @@ int main() {
     stream= fopen("../input.txt", "r");
     if(!stream) return -1;
     printf("File open\n");
-    
-    char letter;
+
+    char* numbers[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    int numbersSize[] = {4, 3, 3, 5, 4, 4, 3, 5, 5, 4};
+    int maxSize=5;
+
+    char* line;
+    size_t lineLen;
     Link first={0,0};
     Link* current=&first;
-    while(fread(&letter,1,1,stream)){
-        if(48<=letter && letter<=57) {
-            if(current->value<10)
-                current->value = (letter-48)*10;
-            else
-                current->value+=-current->value%10+letter-48;
-        }else if(letter==10){
-            if( current->value%10==0 ) current->value+=current->value/10;
-            Link* _tmp= calloc(1,sizeof(FILE));
-            current->next=_tmp;
-            current=_tmp;
+    while (getline(&line, &lineLen, stream) != -1) {
+        char letter='a';
+        for(size_t i = 0; letter!='\0';++i){
+            letter=line[i];
+            if ('0' <= letter && letter <= '9') {
+                if(current->value==0) current->value = (letter - 48) * 10;
+                else
+                    current->value += -current->value % 10 + letter - '0';
+                }else if ('a' <= letter && letter <= 'z'){
+                size_t len=lineLen-i;
+                if(len<2) continue;
+                if(len>maxSize) len=maxSize;
+                char number[len];
+                strncpy(number, &line[i], len);
+                for(int j=0;j<10; ++j)
+                    if (!memcmp(number,numbers[j],len<numbersSize[j]?len:numbersSize[j]))
+                        if(current->value==0) current->value = j * 10;
+                        else current->value += -current->value%10 + j;
+            }
         }
+        printf("\n %s %d\n",line,current->value);
+        if( current->value%10==0 ) current->value+=current->value/10;
+        Link* _tmp= calloc(1,sizeof(Link));
+        current->next=_tmp;
+        current=_tmp;
     }
-    if( current->value%10==0 ) current->value+=current->value/10;//Last one
-
+    free(line);
     fclose(stream);
     printf("Read done\n");
 
